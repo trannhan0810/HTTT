d2d8b2d1ab9983b9d278b908515e93e6a818dc0c
trannhan0810 <trannhan08101999@gmail.com>
2021-01-29 13:49:38 +0700

    YEEEEEEEHHHHHHH x2

--- AHP.py
+++ AHP.py
@@ -1,5 +1,5 @@
 import numpy as np 
-np.set_printoptions(precision=3)
+np.set_printoptions(formatter={'float':lambda x:"{0:0.3f}".format(x)})
 
 def fill(matrix):
     m_len = len(matrix) 
@@ -22,30 +22,18 @@ def fill(matrix):
 
 def normalize(comparisonMatrix):
     matrix = comparisonMatrix.copy()
-    length = len(matrix)
-    for i in range(0, length):
-        sumColumn = np.sum(matrix[:,i], axis=0)
-        for j in range(0, length):
-            matrix[j][i] /= sumColumn
-    return matrix
+    return matrix/np.sum(matrix, axis=0)
 
-def getW(matrix):
-    result = []
-    for i in range(0, len(matrix)):
-        result.append(np.average(matrix[i], axis=0))
-    return np.array(result)
+def getWeightVector(matrix):
+    w = np.sum(matrix, axis=1)
+    return normalize(w)
 
 def getConsistancyMeasure(matrix, w):
-    W = matrix.dot(w)
-    for i in range(0, len(matrix)):
-        W[i] = W[i]/w[i]
-    return W
+    return matrix.dot(w)/w
 
+RI = [0, 0, 0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.46, 1.49]
 def getCR(CM, n):
-    if n>10:
-        print("n must <= 10")
-        return 0
-    RI = [0, 0, 0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.46, 1.49]
+    if n>10:print("n must <= 10");return 0;
     CI = (np.max(CM) - n)/(n-1)
     CR = CI/RI[n]
     return CR
@@ -53,11 +41,10 @@ def getCR(CM, n):
 def calculate(matrix):
     filled = fill(matrix)
     normalized = normalize(filled)
-    w = getW(normalized)
+    w = getWeightVector(normalized)
     CM = getConsistancyMeasure(filled, w)
     CR= getCR(CM, 3)
     return w, CR
-
 
 #=========================================================================
 if __name__ == '__main__':
@@ -68,26 +55,25 @@ if __name__ == '__main__':
     lifeQuality = np.array([[0, 0.33,0.5 ],  [3, 0, 2], [0, 0,  0]], dtype=float)
     interest    = np.array([[1, 0.5, 0.33],  [2, 0, 2], [3, 0.5,0]], dtype=float)
     nearness    = np.array([[1, 0.25,1   ],  [0, 0, 4], [0, 0,  0]], dtype=float)
-    m = [ salary, lifeQuality, interest, nearness ]
+    alternatives = [ salary, lifeQuality, interest, nearness ]
     n = []
     o = []
-    for i in range(0, len(m)):
-        priorities, inconsistency = calculate(m[i])
+    for i in range(0, len(alternatives)):
+        priorities, inconsistency = calculate(alternatives[i])
         n.append(priorities)
         o.append(inconsistency)
     n = np.array(n)
-    criteria = np.array([[1, 5, 2, 4], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
-    priorities, inconsistency = calculate(criteria)
+    criterias = np.array([[1, 5, 2, 4], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
+    priorities, inconsistency = calculate(criterias)
     priorities = np.array(priorities)
-    print(priorities.shape)
-    print(n.shape)
     x = priorities.dot(n)
 
     print("==================")
     print("Inconsistency: ")
-    for i in range(0, len(m)):
-        print(criteria_name[i]+ ": "+ str(o[i]))
+    for i in range(0, len(criterias)):
+        print(criteria_name[i]+ ": "+ "{0:0.3f}".format(o[i]))
     print("criteria: "+ str(inconsistency))
     print("==================")
     for i in range(0, len(x)):
-        print(jobs[i] + ": " + str(x[i]) )
+        print(jobs[i] + ": ", end="")
+        print("{0:0.3f}".format(x[i])) 
--- fuzzyAHP.py
+++ fuzzyAHP.py
@@ -1,47 +1,121 @@
 import numpy as np 
-from AHP import fill, normalize, getW
+from AHP import fill, normalize, getWeightVector
+np.set_printoptions(formatter={'float':lambda x:"{0:0.2f}".format(x)})
 
 
-def fuzzification(listComparisonMatrix):
-    for i in range(0, len(listComparisonMatrix)):
-        listComparisonMatrix[i] = fill(listComparisonMatrix[i])
-        #print(listComparisonMatrix[i])
-  
-    L = np.min(listComparisonMatrix, 0)
-    M = np.average(listComparisonMatrix, 0)
-    U = np.max(listComparisonMatrix, 0)
+def show(matrix, name="---------"):
+    print("---"+name+"----")
+    if(matrix.ndim >4): print("Error, can't show matrix with high than 4 dimension"); return;
+    if(matrix.ndim ==4):
+        for i in range(0,3): show(matrix[i]); 
+        return
+    if(matrix.ndim >=3):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                print(matrix[i][j], end=", ")
+            print("")
+    else:
+        print(matrix)
+
+def fillAllComparisonMatrix(matrixs):
+    print(matrixs.shape)
+    for i in range(0, len(matrixs)):
+        matrixs[i] = fill(matrixs[i])
+    return matrixs
+
+def aggegate(listComparisonMatrix):
+    L = np.min(listComparisonMatrix, axis = 0)
+    M = np.average(listComparisonMatrix, axis = 0)
+    U = np.max(listComparisonMatrix, axis = 0)
     return np.dstack((L, M, U))
 
-def printTensor(matrix):
-    h, w, d = matrix.shape
-    for i in range(0, h):
-        for j in range(0, w):
-            print(matrix[i][j], end=", ")
-        print("")
+def fuzzyfication(M):
+    s = (M.shape)
+    L = np.maximum(M-2, np.full(s, 1))
+    U = np.minimum(M+2, np.full(s, 9))
+    return np.dstack((L, M, U))
 
-def fuzz(x):
-    l = x-2 if (x-2)>=1 else 1
-    u = x+2 if (x+2)<=9 else 9
-    return np.array([l, x, u])
+def defuzzyfication(fuzzyMatrix, alpha = 0.5, beta = 0.5):
+    alpha_l = fuzzyMatrix[:,:,0] + alpha*(fuzzyMatrix[:,:,1] - fuzzyMatrix[:,:,0])
+    alpha_r = fuzzyMatrix[:,:,2] - alpha*(fuzzyMatrix[:,:,2] - fuzzyMatrix[:,:,1])
+    alpha_matrix = np.dstack((alpha_l, alpha_r))
+    #show(alpha_matrix, "alpha")
 
-def aggregateIndividualPreference(listPrefernce):
-    listPrefernce = fuzz(listPrefernce)
+    beta_matrix = np.add(np.multiply(alpha_matrix[:,:,0],beta),np.multiply(alpha_matrix[:,:,1],(1-beta)))
+    #show(beta_matrix, "beta")
+    
+    return beta_matrix
 
-listComparisonMatrix = [
-    np.array([[0,4,3,2], [0,0,0.8,0.7], [0,0,0,0.6],[0,0,0,0]], dtype=float),
-    np.array([[0,4.2,3,1.5], [0,0,0.85,0.7], [0,0,0,0.65],[0,0,0,0]], dtype=float),
-    np.array([[0,3.9,2.8,2.3], [0,0,0.7,0.7], [0,0,0,0.65],[0,0,0,0]], dtype=float),
-]
+def reorder(matrix):
+    if(matrix.ndim != 3): print("ERROR"); return;
+    L = np.min([matrix[:,:,0], matrix[:,:,1], matrix[:,:,2]], axis = 0)
+    U = np.max([matrix[:,:,0], matrix[:,:,1], matrix[:,:,2]], axis = 0)
+    M = matrix[:,:,0] + matrix[:,:,1] + matrix[:,:,2] - L - U
+    return np.dstack((L, M, U))
 
-prefernce1 = np.array([[2, 1, 3, 4], [4, 3, 1, 2], [3, 2, 2, 2]])
-prefernce2 = np.array([[3, 3, 1, 3], [4, 1, 2, 3], [4, 4, 1, 1]])
-prefernce3 = np.array([[2, 2, 1, 5], [2, 3, 2, 3], [3, 3, 3, 1]])
-listPreference = np.array([prefernce1, prefernce2, prefernce3])
-
-print(fuzz(prefernce1))
 #==================================================================
-fuzzied = fuzzification(listComparisonMatrix)
+D1 = np.array([[0.0, 4.0, 3.0, 2.0], [0.0, 0.0, 0.8, 0.7], [0.0, 0.0, 0.0, 0.60], [0, 0, 0, 0]], dtype=float)
+D2 = np.array([[0.0, 4.2, 3.0, 1.5], [0.0, 0.0, 0.85,0.7], [0.0, 0.0, 0.0, 0.65], [0, 0, 0, 0]], dtype=float)
+D3 = np.array([[0.0, 3.9, 2.8, 2.3], [0.0, 0.0, 0.7, 0.7], [0.0, 0.0, 0.0, 0.65], [0, 0, 0, 0]], dtype=float)
+listComparisonMatrix = np.array([D1, D2, D3])
+
+alternative1 = np.array([[2, 1, 3, 4], [4, 3, 1, 2], [3, 2, 2, 2]])
+alternative2 = np.array([[3, 3, 1, 3], [4, 1, 2, 3], [4, 4, 1, 1]])
+alternative3 = np.array([[2, 2, 1, 5], [2, 3, 2, 3], [3, 3, 3, 1]])
+listPreference = np.array([fuzzyfication(alternative1), fuzzyfication(alternative2), fuzzyfication(alternative3)])
+
+listComparisonMatrix = fillAllComparisonMatrix(listComparisonMatrix)
+fuzzied = aggegate(listComparisonMatrix)
+show(fuzzied, "fuzzyComaprisonMatrix")
+
 fuzzied_normalized = normalize(fuzzied)
-w = getW(fuzzied_normalized)
+show(fuzzied_normalized, "normalizedFuzzyComaprisonMatrix")
 
+w = getWeightVector(fuzzied_normalized)
+show(w, "criteriaWeightVector")
 
+show(listPreference, "fuzzyAlternativeMatrix")
+G_L = np.min(listPreference[:,:,:,0], axis=0)
+G_M = np.average(listPreference[:,:,:,1], axis=0)
+G_U = np.max(listPreference[:,:,:,2], axis=0)
+G_matrix = np.dstack((G_L, G_M, G_U))
+show(G_matrix, "G_matrix")
+
+a_matrix = np.empty(G_matrix.shape)
+for j in range(0, len(a_matrix[0])):
+    sum_column_normalize = np.sqrt(np.sum(G_matrix[:,j]**2, axis=0))
+    for i in range(0, len(a_matrix)):
+        a_matrix[i][j] = G_matrix[i][j]/sum_column_normalize
+show(a_matrix, "a_matrix")
+
+a_matrix_reorder = reorder(a_matrix)
+show(a_matrix_reorder, "a_matrix_reorder")
+
+h_matrix = a_matrix.copy()
+for i in range(0, len(h_matrix[0])):
+    h_matrix[:,i] *= w[i]
+show(h_matrix, "h_matrix")
+
+h_matrix_reorder = reorder(h_matrix)
+show(h_matrix_reorder, "h_matrix_reorder")
+
+h_defuzzy = defuzzyfication(h_matrix_reorder, alpha=0.6, beta=0.5)
+show(h_defuzzy, "defuzzy")
+
+h_max = np.max(h_defuzzy, axis=0)
+h_min = np.min(h_defuzzy, axis=0)
+h_beta1 = h_defuzzy.copy()
+h_beta2 = h_defuzzy.copy()
+for i in range(0, len(h_defuzzy)):
+    h_beta1[i]=h_beta1[i]-h_max
+    h_beta2[i]=h_beta2[i]-h_min
+show(h_beta1, "h1")
+show(h_beta2, "h2")
+S_max = np.sqrt(np.sum(h_beta1**2, axis=1)).T
+S_min = np.sqrt(np.sum(h_beta2**2, axis=1)).T
+S = np.vstack((S_max, S_min))
+S = np.transpose(S, axes=(1,0))
+show(S, "S_matrix")
+
+R = S[:,1]/(S[:,1]+S[:,0])
+show(R, "R_matrix")
